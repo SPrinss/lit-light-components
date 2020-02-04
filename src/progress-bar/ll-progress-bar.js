639a2cb842bc8702c3bdec05a57b9e42fdd97584
Wouter Vroege <wouter@woutervroege.nl>
2020-02-04 14:02:36 +0100

    upgraded progress-bar
    Signed-off-by: Wouter Vroege <wouter@woutervroege.nl>

--- src/progress-bar/ll-progress-bar.js
+++ src/progress-bar/ll-progress-bar.js
@@ -1,6 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
-import { BorderRadius } from '../styles';
-import { Colors, Gradients } from '../styles';
+import { LitElementLight, html } from 'lit-element-light';
 
 /**
  * Conamore UI Progress bar Component
@@ -9,17 +7,10 @@ import { Colors, Gradients } from '../styles';
  * @cssprop --ll-progress-bar-height - Bar Height
  * @cssprop --ll-progress-bar-color - Bar Color
  */
-class Main extends LitElement {
-  render() {
+class Main extends LitElementLight {
+  get template() {
     return html`
-      <style>
-        :host {
-          display: block;
-          ${Colors}
-          ${BorderRadius}
-          ${Gradients}
-        }
-      </style>
+      <link rel="stylesheet" href="src/progress-bar/ll-progress-bar.css">
       <div id="progress-bar">
         ${this._maxArray.map((item, i) => html`<span ?data-active="${(this.value > i)}"></span>`)}
       </div>
@@ -70,59 +61,6 @@ class Main extends LitElement {
     const values = [];
     for(let i=0;i<=(this.max || 0)-1 ; i++) values.push(i);
     return values;
-  }
-
-  static get styles() {
-    return css`
-
-      #progress-bar {
-        width: 100%;
-        height: 100%;
-        z-index: 2;
-        overflow: hidden;
-        display: flex;
-        padding: 0 3px;
-        margin: 3px 0;
-        box-sizing: border-box;
-        height: var(--ll-progress-bar-height, 3px);
-      }
-
-      span {
-        flex: 1;
-        margin: 0 3px;
-        border-radius: var(--ll-border-radius, 5px);
-        overflow: hidden;
-        position: relative;
-      }
-
-      :host([solid]) span {
-        margin: 0;
-        border-radius: 0;
-      }
-
-      span:before, span:after {
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        content: '';
-        position: absolute;
-      }
-
-      span:before {
-        opacity: 0.35;
-      }
-
-      span:after {
-        background: var(--ll-progress-bar-color, var(--ll-gradient-greenblue));
-        opacity: 0.1;
-        transition: 0.5s opacity ease-in-out;
-      }
-
-      span[data-active]:after {
-        opacity: 1;
-      }
-    `;
   }
 
 }
